4f86e469e438cdd1ab90d4d9ed2d886fc1b8e42a
markoarsyc <markoarsyc@gmail.com>
2026-06-01 11:54:26 +0200

    Dokerizovan front za dev okruzenje

--- stanovi-backend/src/main.ts
+++ stanovi-backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
